c80197170509bc398d8e325c44a075575f6518fd
Wes Madaj <wtmadaj@gmail.com>
2020-02-29 21:43:09 -0500

    added files for  page with different overlay

---
+++ Scripts/contact-in-overlay-script.js
@@ -0,0 +1,17 @@
+/* Open when clicking on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100px";
+  document.getElementById("body").style.marginLeft = "100px";
+  document.getElementById("icon-area").style.marginLeft = "20px";
+  document.getElementById("nav-button").style.color = "red"; // make the button stay red
+  document.getElementById("nav-button").style.transform = "scale(1.5)"; // make the button stay enlarged to show that it's active
+}
+
+/* Close when clicking on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+  document.getElementById("body").style.marginLeft = "0%";
+  document.getElementById("icon-area").style.marginLeft = "0%";
+  document.getElementById("nav-button").style.color = ""; // reset the styling to normal per stylesheet
+  document.getElementById("nav-button").style.transform = ""; // reset the styling to normal per stylesheet
+}
