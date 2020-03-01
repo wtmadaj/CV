c2995e90c05bbe42f58e1c6236c8fc0d642a2f08
Wes Madaj <wtmadaj@gmail.com>
2020-02-29 21:51:42 -0500

    name cleanup

--- Scripts/contact-in-overlay-script.js
+++ Scripts/contact-in-overlay-script.js
@@ -2,7 +2,7 @@
 function openNav() {
   document.getElementById("myNav").style.width = "100px";
   document.getElementById("body").style.marginLeft = "100px";
-  document.getElementById("icon-area").style.marginLeft = "20px";
+  document.getElementById("menu-button-area").style.marginLeft = "20px";
   document.getElementById("nav-button").style.color = "red"; // make the button stay red
   document.getElementById("nav-button").style.transform = "scale(1.5)"; // make the button stay enlarged to show that it's active
 }
@@ -11,7 +11,7 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
   document.getElementById("body").style.marginLeft = "0%";
-  document.getElementById("icon-area").style.marginLeft = "0%";
+  document.getElementById("menu-button-area").style.marginLeft = "0%";
   document.getElementById("nav-button").style.color = ""; // reset the styling to normal per stylesheet
   document.getElementById("nav-button").style.transform = ""; // reset the styling to normal per stylesheet
 }
